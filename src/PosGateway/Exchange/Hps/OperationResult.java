a83dd3bc6c49b4f18e6cca79e71265003fa3d43b
russell.everett <russell.everett@e-hps.com>
2015-07-07 09:13:27 -0400

    removing files left behind by the deployment

--- src/PosGateway/Exchange/Hps/OperationResult.java
+++
@@ -1,8 +0,0 @@
-package PosGateway.Exchange.Hps;
-
-public class OperationResult<T> {
-    public java.lang.Exception Exception;
-    public T Result;
-    public Object Tag;
-    public String MethodName;
-}
